6abb2e1861fa3ce48c01c784848cca4f48dbc27d
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-05-14 19:30:54 -0400

    taking notes while watching the videos in wes bos js course

---
+++ public/closures.js
@@ -0,0 +1,2 @@
+// A closure is the ability to be able to access the parent scope from the child scope
+  // after parent Function has been terminated
---
+++ public/the-dom.js
@@ -0,0 +1,4 @@
+// NodeList is not an array but is a list of items
+
+document.querySelector()
+document.querySelectorAll()
