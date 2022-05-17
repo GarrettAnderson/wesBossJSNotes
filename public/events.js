4cfebf4827a99bfdde8525da8175895892314c58
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-05-17 18:42:18 -0400

    taking notes while watching the videos for wes bos js course. currently learning about events

--- public/events.js
+++ public/events.js
@@ -23,3 +23,23 @@
 // console.dir() will give more detail to console message
 
 // always give your inputs a name attribute when building forms
+
+
+// various types of events:
+// keypup
+// keydown
+// focus
+// blur
+// mouseover
+// etc.
+
+
+// Accessibility
+
+// do not use a href elements for buttons
+// a href for links that navigate to another page
+// if you will use an element as a button always include the role attribute
+  // role='button'
+
+//every key on the keyboard has its own event code
+  // event.key
