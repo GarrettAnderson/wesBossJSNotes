ae3ce7e70c6ea74cfae6d4d1acba76fa54309808
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-05-17 18:34:16 -0400

    taking notes while watching the videos for wes bos js course. currently learning about events

--- public/events.js
+++ public/events.js
@@ -8,5 +8,18 @@
 
 // Propagation
 // button event bubbles up to the top of the page??
+    // to the parent elements until it reaches the document object
 // event can still be triggered so must attach the method
-// event.stopPropagation()
+    // event.stopPropagation()
+// so that other events aren't triggered
+
+
+// the 'this' keyword is NOT scoped to arrow functions
+// dont use 'this' keyword on event callbacks
+
+// use preventDefault method
+
+
+// console.dir() will give more detail to console message
+
+// always give your inputs a name attribute when building forms
