a9cffb45d238c080bf101b9201828bdf60dc32f8
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-05-17 18:22:16 -0400

    taking notes while watching the videos for wes bos js course. currently learning about events

--- public/events.js
+++ public/events.js
@@ -1,4 +1,12 @@
 // Diff between event.target vs event.currentTarget
 // helful when there are children of a buttons
 // event.currentTarget is the element that triggers the event addEventListener
-// 
+//
+//
+//
+
+
+// Propagation
+// button event bubbles up to the top of the page??
+// event can still be triggered so must attach the method
+// event.stopPropagation()
